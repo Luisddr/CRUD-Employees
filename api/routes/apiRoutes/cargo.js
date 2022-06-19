ca69058522c1d36dfa399779c6ad8523ae53197a
Luis Carlos <luisdediosr@gmail.com>
2022-06-18 20:46:27 -0500

    Form & delete done

--- api/routes/apiRoutes/cargo.js
+++ api/routes/apiRoutes/cargo.js
@@ -7,33 +7,57 @@ const router = Router();
 
 
 router.post('/', async(req, res)=>{
-    const cargo = await Cargo.create(req.body)
+  try{
 
+    const cargo = await Cargo.create(req.body)
+    
     
     res.json(cargo)
+  }
+  catch(err){
+    console.error(err)
+  }
 })
 
 
 router.get('/', async (req, res)=>{
-    const cargos = await  Cargo.findAll({include: Empleado})
+  try{
 
+    const cargos = await  Cargo.findAll({include: Empleado})
+    
     res.send(cargos)
-})
+  }
+  catch(err){
+    console.error(err)
+  }
+  })
 
 
 router.put('/:id', async (req, res)=>{
+  try{
+
     await Cargo.update(req.body, {
-        where: { id: req.params.id },
-      });
-      res.json({ success: "Position has been modiefied" });
-})
+      where: { id: req.params.id },
+    });
+    res.json({ success: "Position has been modiefied" });
+  }
+  catch(err){
+    console.error(err)
+  }
+  })
 
 
 router.delete("/:id", async (req, res) => {
+  try{
+
     await Cargo.destroy({
       where: { id: req.params.id },
     });
     res.json({ success: "item has been deleted" });
+  }
+  catch(err){
+    console.error(err)
+  }
   });
 
 
--- api/routes/apiRoutes/empleado.js
+++ api/routes/apiRoutes/empleado.js
@@ -7,38 +7,62 @@ const router = Router();
 
 
 router.post('/', async(req, res)=>{
+  try{
+
     const empleado = await Empleado.create(req.body);
     const {cargo} = req.body;
-
-   let workerPosition  = await Cargo.findOne({
-        where: {name : cargo}
+    
+    let workerPosition  = await Cargo.findOne({
+      where: {name : cargo}
     })
-
+    
     await empleado.addCargo(workerPosition)
     res.json(empleado)
+  }
+  catch(err){
+    console.error(err)
+  }
 })
 
 
 router.get('/', async (req, res)=>{
-    const empleados = await Empleado.findAll({include: Cargo})
+  try{
 
+    const empleados = await Empleado.findAll({include: Cargo})
+    
     res.send(empleados)
+  }
+  catch(err){
+    console.error(err)
+  }
 })
 
 
 router.put('/:id', async (req, res)=>{
+  try{
+
     await Empleado.update(req.body, {
-        where: { id: req.params.id },
-      });
-      res.json({ success: "Employee has been modiefied" });
+      where: { id: req.params.id },
+    });
+    res.json({ success: "Employee has been modiefied" });
+  }
+  catch(err){
+    console.error(err)
+  }
 })
 
 
 router.delete("/:id", async (req, res) => {
+  try{
+
     await Empleado.destroy({
       where: { id: req.params.id },
     });
     res.json({ success: "item has been deleted" });
+  }
+  catch(err){
+    console.error(err)
+  }
   });
 
 
